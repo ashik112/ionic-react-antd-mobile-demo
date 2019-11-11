bab57a6ad8c02696988da7e5a4ef7563e3fd45e0
Md. Ashekur Rahman <ashekur.rahman@eongroup.net.bd>
2019-11-11 15:31:36 +0600

    use antd NavBar & Card

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,28 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import { NavBar, Icon, Card } from 'antd-mobile';
 import CalendarDemo from '../components/antd/CalendarDemo';
 
 
 const Home = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Ionic Antd Mobile</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      <NavBar
+        mode="dark"
+        rightContent={[
+          <Icon key="0" type="search" style={{ marginRight: '16px' }} />,
+          <Icon key="1" type="ellipsis" />,
+        ]}
+      >Ionic + Antd Mobile</NavBar>
       <IonContent className="ion-padding">
-        <p>
-          If you get lost, the{' '}
-          <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/">
-            docs
-          </a>{' '}
-          will be your guide.
-        </p>
-        <CalendarDemo/>
+        <Card>
+          <Card.Header
+            title="Calendar"
+          />
+          <Card.Body>
+            <CalendarDemo />
+          </Card.Body>
+        </Card>
       </IonContent>
     </IonPage>
   );
